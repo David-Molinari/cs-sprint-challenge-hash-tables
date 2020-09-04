ef9143965f43baef907ccd72088e942496a1e673
David Molinari <david.m.molinari@gmail.com>
2020-09-04 11:49:46 -0400

    1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,14 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    dictionary0 ={}
+    counter = 0
+    answer = ()
+    for w in weights:
+        found = dictionary0.get(limit - w)
+        if found != None:
+            answer = (counter, found)
+            return answer
+        dictionary0[w] = counter
+        counter += 1
     return None
